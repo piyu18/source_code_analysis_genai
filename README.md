3773184ec58d6dde792d3818162cc1f1c6105170
priya <priya1803singh@gmail.com>
2024-04-11 01:13:18 +0530

    notebook code done

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # source_code_analysis_genai
+
+### Create new environment
+```bash
+conda create -p venv python=3.8 -y
+```
+
+### Activate environment
+```bash
+conda activate venv/
+```
+
+### Install dependencies
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Source Code Analysis using OpenAI',
+    version= '0.0.1',
+    author= 'Priya',
+    author_email= 'priya1803singh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
